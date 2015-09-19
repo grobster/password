8064b8d193cc362da98f55eff4e364365a6a1da5
grobster <jquarles@grobster.com>
2015-09-19 17:27:40 -0400

    added upper character array to Password class

--- source/com/grobster/password/Password.java
+++ source/com/grobster/password/Password.java
@@ -4,12 +4,17 @@ public class Password {
 	private int passwordLength;
 	private String password;
 	public static final char[] LOWER_LETTERS = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	public static final char[] UPPER_LETTERS = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 	public static final char[] NUMBERS = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
-	public static final char[][] ALL_CHARS = {LOWER_LETTERS, NUMBERS};
+	public static final char[][] ALL_CHARS = {LOWER_LETTERS, UPPER_LETTERS, NUMBERS};
 	
 	public Password(int passwordLength) {
-		this.passwordLength = passwordLength;
+		if (passwordLength > 0) {
+			this.passwordLength = passwordLength;
+		}
 	}
+	
+	public Password() {}
 	
 	
 	public String createPassword() {
@@ -26,7 +31,9 @@ public class Password {
 	}
 	
 	public void setPasswordLength(int passwordLength) {
-		this.passwordLength = passwordLength;
+		if (passwordLength > 0) {
+			this.passwordLength = passwordLength;
+		}
 	}
 	
 	public int getPasswordLength() {

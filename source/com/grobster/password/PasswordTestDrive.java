c0cd86f76964dd16f8b90f976bfba4c145231b39
grobster <jquarles@grobster.com>
2015-09-19 17:44:37 -0400

    added test class to PasswordTestDrive

--- source/com/grobster/password/PasswordTestDrive.java
+++ source/com/grobster/password/PasswordTestDrive.java
@@ -2,7 +2,9 @@ package com.grobster.password;
 
 public class PasswordTestDrive {
 	public static void main(String[] args) {
-		
+		Password password = new Password(11);
+		String p = password.createPassword();
+		System.out.println(p);
 	}
 	
 }

46eeae766fed6cc441e8324384a963d2f78f3c39
grobster <jquarles@grobster.com>
2015-09-20 07:47:07 -0400

    overrode createPassword() in AllCharactersPassword class

--- source/com/grobster/password/AllCharactersPassword.java
+++ source/com/grobster/password/AllCharactersPassword.java
@@ -1,6 +1,14 @@
 package com.grobster.password;
 
 public class AllCharactersPassword extends PasswordRequirements {
+	private AbstractPassword password;
 	
+	public AllCharactersPassword(AbstractPassword password) {
+		this.password = password;
+	}
+	
+	public String createPassword() {
+		return null;
+	}
 	
 }
